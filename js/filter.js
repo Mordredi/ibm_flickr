57185f44c3ee073b2da13aea82907d18d2565f04
Arthur <arthur.p.wright@gmail.com>
2015-06-19 20:37:14 -0400

    Can now add filter class to photos

--- js/filter.js
+++ js/filter.js
@@ -2,6 +2,6 @@ $(document).on('ready', function(){
   $('.available').on('click', function(e){
     e.preventDefault();
     var filter = $(this).data('filter');
-    $(this).addClass(filter);
+    $('.photos').addClass(filter);
   });
 });
