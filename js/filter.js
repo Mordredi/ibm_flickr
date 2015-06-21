ead67629eaa323955b3c97a308bf927f2264381a
Arthur <arthur.p.wright@gmail.com>
2015-06-20 20:13:09 -0400

    Even or odd filter will be removed when opposite is selected

--- js/filter.js
+++ js/filter.js
@@ -3,6 +3,16 @@ $(document).on('ready', function(){
     var self = $(this);
     e.preventDefault();
     var filter = self.data('filter');
+    if (filter === 'odd' && $('.photos').hasClass('even')) {
+      $('.photos').removeClass('even');
+      var button = $('.filter[data-filter="even"]').detach();
+      $('.available').append(button);
+    } else if (filter === 'even' && $('.photos').hasClass('odd')) {
+      $('.photos').removeClass('odd');
+      var button = $('.filter[data-filter="odd"]').detach();
+      $('.available').append(button);
+    }
+
     if ($('.photos').hasClass(filter)) {
       $('.photos').removeClass(filter);
       var button = self.detach();
